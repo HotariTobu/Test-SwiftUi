82d9f6af3ab98e085ceb00f3b8e752d11e450e22
HotariTobu <hotari24tools@gmail.com>
2022-08-07 08:27:46 +0900

    rename: SwiftUI_Test -> Test_SeiftUI

---
+++ Shared/Test_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_SwiftUIApp.swift
+//  Shared
+//
+//  Created by HotariTobu on 2021/08/06.
+//
+
+import SwiftUI
+
+@main
+struct Test_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
